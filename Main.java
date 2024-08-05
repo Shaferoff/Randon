bab039c90e5ae1072f3993001301c5f165f19328
Shaferoff <shaferoff11@gmail.com>
2024-08-05 12:55:59 +0300

    Random [0, 1000_000]
    Три случайных числа

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i, x;
+        for ( i=1; i<=3; i++) {
+            x=(int)(Math.random()*1000001);
+            System.out.println(x);
+        }
+    }
+}
